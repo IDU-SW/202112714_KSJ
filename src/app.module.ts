9131692ae9872c0b6cb6c997c2fce4060859e97f
zellytozelly <zellytozelly@gmail.com>
2021-06-19 14:46:50 +0900

    FEAT: create song

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SongController } from './song/song.controller';
+import { SongModule } from './song/song.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [SongModule],
+  controllers: [AppController, SongController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/song/song.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { SongService } from './song.service';
+
+@Module({
+  providers: [SongService]
+})
+export class SongModule {}
