b57a4078bf79b587401173ab97d6f715f54a4797
zellytozelly <zellytozelly@gmail.com>
2021-06-22 01:12:20 +0900

    FEAT: add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,16 @@ import { Album } from './album/table/album.table';
 import { SongModule } from './song/song.module';
 import { ArtistModule } from './artist/artist.module';
 import { Artist } from './artist/table/artist.table';
+import 'dotenv/config';
 
 @Module({
   imports: [SequelizeModule.forRoot({
     dialect: 'postgres',
-    host: '3.34.148.155',
+    host: process.env.DB_HOST,
     port: 5432,
-    username: 'dev',
-    password: '1234',
-    database: 'ksj',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     dialectOptions: { charset: 'utf8mb4', dateStrings: true, typeCast: true },
     synchronize: true,
     autoLoadModels: true,
